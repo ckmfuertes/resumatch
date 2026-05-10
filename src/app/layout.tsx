1ae47b1b523da81a41863b61532c38478180cb2c
Cherwen Kirk Fuertes <214774830+ckmfuertes@users.noreply.github.com>
2026-05-10 16:37:09 +0800

    feat: add toast component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Noto_Serif_Georgian, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
